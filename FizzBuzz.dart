2b9a7745e977bcd78d3ccea24ed92900e332be52
thierrybayi <thierrybayi@gmail.com>
2013-09-09 05:35:23 -0400

    add the first version of theDart_FizBuzz Project

---
+++ FizzBuzz.dart
@@ -0,0 +1,38 @@
+main() {
+
+  for (int i = 1; i < 101; i++) {
+
+    if ((i % 3 != 0 && i % 5 != 0) && (i % 3 != 0) && (i % 5 != 0)) {
+
+      print(i);
+
+    } else {
+
+      if (i % 3 == 0 && i % 5 == 0) {
+
+        print('FizzBuzz');
+
+      } else {
+
+        if (i % 3 == 0) {
+
+          print('Fizz');
+
+        } else {
+
+          print('Buzz');
+
+          }
+
+        }
+
+      }
+
+   }   
+}                                 
+
+    
+        
+            
+      
+  
---
+++ README.md
@@ -0,0 +1,9 @@
+
+##FizzBuzz1
+
+**Categories**: education, Dart.
+
+**Description**: Learning about coding in Dart.
+
+by Thierry Bayi
+
