b0970faf802afb3aaebf90307eaa55bc624fb94e
thierrybayi <thierrybayi@gmail.com>
2013-09-09 15:05:05 -0400

    add the first version of the Dart_FizzBuzz Project

--- FizzBuzz.dart
+++ FizzBuzz.dart
@@ -1,38 +1,45 @@
 main() {
-
+  
   for (int i = 1; i < 101; i++) {
 
     if ((i % 3 != 0 && i % 5 != 0) && (i % 3 != 0) && (i % 5 != 0)) {
 
       print(i);
 
+    } else if (i % 3 == 0 && i % 5 == 0) {
+      
+      print('FizzBuzz');
+      
+    } else if (i % 3 == 0) {
+      
+      print('Fizz');
+      
     } else {
-
-      if (i % 3 == 0 && i % 5 == 0) {
-
-        print('FizzBuzz');
-
-      } else {
-
-        if (i % 3 == 0) {
-
-          print('Fizz');
-
-        } else {
-
-          print('Buzz');
-
-          }
-
-        }
-
-      }
-
-   }   
-}                                 
-
+      
+      print('Buzz');
+      
+    }
     
-        
+  }
+  
+}
+    
+
+      
+
+       
+
+  
+
+      
+
+         
+
+      
+
+      
+
+      
             
       
   
