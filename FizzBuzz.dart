4729a04691494ddcfcf808e1595e9351d5bcf5d3
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-11 14:46:05 -0400

    format code; add doc

--- FizzBuzz.dart
+++ FizzBuzz.dart
@@ -1,45 +1,33 @@
 main() {
-  
   for (int i = 1; i < 101; i++) {
-
-    if ((i % 3 != 0 && i % 5 != 0) && (i % 3 != 0) && (i % 5 != 0)) {
-
-      print(i);
-
-    } else if (i % 3 == 0 && i % 5 == 0) {
-      
+    if (i % 3 == 0 && i % 5 == 0) {
       print('FizzBuzz');
-      
     } else if (i % 3 == 0) {
-      
       print('Fizz');
-      
-    } else {
-      
+    } else if (i % 5 == 0) {
       print('Buzz');
-      
+    } else {
+      print(i);
     }
-    
   }
-  
 }
-    
 
-      
 
-       
 
-  
 
-      
 
-         
 
-      
 
-      
 
-      
-            
-      
-  
+
+
+
+
+
+
+
+
+
+
+
+
