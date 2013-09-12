bdc004dd16aa83da99ad0b97bc13400ea6040bfa
thierrybayi <thierrybayi@gmail.com>
2013-09-12 01:43:10 -0400

    add the second version of dart_fizzbuzz

--- FizzBuzz.dart
+++ FizzBuzz.dart
@@ -1,11 +1,11 @@
 main() {
   for (int i = 1; i < 101; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-      print('FizzBuzz');
+      print('$i'':FizzBuzz');
     } else if (i % 3 == 0) {
-      print('Fizz');
+      print('$i'':Fizz');
     } else if (i % 5 == 0) {
-      print('Buzz');
+      print('$i'':Buzz');
     } else {
       print(i);
     }
